2388fc5adb8840585a6e41400b5568b399db15a6
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-08-16 00:02:59 +0900

    chore: dotenv 설정

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import app from './app';
+require('./dotenv');
 
-const PORT = process.env.PORT || 5678;
+const PORT = process.env.PORT || 6789;
 app.listen(PORT, () => console.log(`listening on port ${PORT}...`));
