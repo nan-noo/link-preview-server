8f761ce0969d5e7cee0d17b4574f67d236f5ce28
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-08-15 22:41:05 +0900

    feat: link preview api 구현
    open graph 태그, meta description, title 태그를 응답값으로 title, description, imageUrl, domainName을 반환

---
+++ src/app.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+
+import type { Query } from 'express-serve-static-core';
+import getPreviewData from './utils/getPreviewData';
+
+export interface TypedRequestQuery<T extends Query> extends Express.Request {
+  query: T;
+}
+
+export type GetLinkPreviewResponseBody = { imageUrl?: string; title: string; description?: string; domainName: string };
+export type GetLinkPreviewRequestParams = { linkUrl: string };
+
+const app = express();
+app.use(
+  cors({
+    origin: true,
+  }),
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const getLinkHtml = async (linkUrl: string) => {
+  const response = await axios.get(linkUrl);
+  return response.data;
+};
+
+app.get('/api/link-preview', async (req: TypedRequestQuery<GetLinkPreviewRequestParams>, res) => {
+  const { linkUrl } = req.query;
+  if (!linkUrl) {
+    console.error('linkUrl이 없음!');
+    return res.status(400).json({ message: 'no reqest data(url)' });
+  }
+
+  try {
+    const rawHtmlString = await getLinkHtml(linkUrl);
+    if (!rawHtmlString) return res.status(400).json({ message: 'wrong url format' });
+
+    const previewData = getPreviewData(rawHtmlString);
+    return res.status(200).json(previewData);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'internal server error' });
+  }
+});
+
+export default app;
---
+++ src/utils/getPreviewData.ts
@@ -0,0 +1,72 @@
+type TagName = string;
+type HtmlString = string;
+
+type AttributeName = 'desc' | 'ogTitle' | 'ogUrl' | 'ogImg' | 'ogDesc';
+type Attributes = {
+  attr: string;
+  name: AttributeName;
+};
+
+const getMatchedTags = (htmlString: HtmlString, tagName: TagName) => {
+  const matchedIterator = htmlString.matchAll(new RegExp(`<${tagName}(.*?)>`, 'g'));
+  const matchedArray = [...matchedIterator];
+
+  return matchedArray.map(matched => matched[0]);
+};
+
+const getTagInnerContent = (htmlString: HtmlString, tagName: TagName) => {
+  const postFixIncluded = htmlString
+    .split(new RegExp(`(<${tagName}>)|(<${tagName}(\s.*?)>)`))
+    .slice(1)
+    .join('');
+
+  const innerContent = postFixIncluded.split(`</${tagName}>`)[0];
+  return innerContent;
+};
+
+const parseHtml = (data: HtmlString) => {
+  const headHtmlString = getTagInnerContent(data, 'head');
+  const title = getTagInnerContent(headHtmlString, 'title');
+  const metaTags = getMatchedTags(headHtmlString, 'meta');
+
+  return { title, metaTags };
+};
+
+const previewAttributes: Array<Attributes> = [
+  { attr: 'name="description"', name: 'desc' },
+  { attr: 'property="og:title"', name: 'ogTitle' },
+  { attr: 'property="og:url"', name: 'ogUrl' },
+  { attr: 'property="og:image"', name: 'ogImg' },
+  { attr: 'property="og:description"', name: 'ogDesc' },
+];
+
+const getPreviewData = (data: HtmlString) => {
+  const pureData = data.replace(/[\r\n]/gm, '');
+  const { title, metaTags } = parseHtml(pureData);
+
+  const defaultMetaContents: Record<AttributeName, string | null> = {
+    desc: null,
+    ogTitle: null,
+    ogUrl: null,
+    ogImg: null,
+    ogDesc: null,
+  };
+
+  const metaContents = previewAttributes.reduce((acc, { attr, name }) => {
+    const metaTag = metaTags.find(tag => tag.includes(attr));
+    if (!metaTag) return acc;
+
+    const content = metaTag.split(/content="/)[1].split('"')[0];
+    acc[name] = content;
+    return acc;
+  }, defaultMetaContents);
+
+  return {
+    title: metaContents.ogTitle ?? title,
+    description: metaContents.ogDesc ?? metaContents.desc,
+    imageUrl: metaContents.ogImg,
+    domainName: metaContents.ogUrl,
+  };
+};
+
+export default getPreviewData;
